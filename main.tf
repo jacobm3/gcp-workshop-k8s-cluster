144d899cb106c3d3c38af337a74e5c36e1862899
Jacob Martinson <jacobm3@gmail.com>
2019-05-15 07:54:57 -0500

    Update `date +%s`

--- main.tf
+++ main.tf
@@ -7,28 +7,38 @@ provider "google" {
   region      = "${var.gcp_region}"
 }
 
-resource "google_container_cluster" "k8sexample" {
-  name               = "${var.cluster_name}"
-  description        = "example k8s cluster"
-  location               = "${var.gcp_zone}"
-  initial_node_count = "${var.initial_node_count}"
-  enable_kubernetes_alpha = "true"
-  enable_legacy_abac = "true"
+resource "google_storage_bucket" "another-bucket" {
+  name     = "yourname-bucket-demo-329902938048302"
+  location = "US"
 
-  master_auth {
-    username = "${var.master_username}"
-    password = "${var.master_password}"
-  }
-
-  node_config {
-    machine_type = "${var.node_machine_type}"
-    disk_size_gb = "${var.node_disk_size}"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
-    ]
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
   }
 }
+
+#resource "google_container_cluster" "k8sexample" {
+#  name               = "${var.cluster_name}"
+#  description        = "example k8s cluster"
+#  location               = "${var.gcp_zone}"
+#  initial_node_count = "${var.initial_node_count}"
+#  enable_kubernetes_alpha = "true"
+#  enable_legacy_abac = "true"
+#
+#  master_auth {
+#    username = "${var.master_username}"
+#    password = "${var.master_password}"
+#  }
+#
+#  node_config {
+#    machine_type = "${var.node_machine_type}"
+#    disk_size_gb = "${var.node_disk_size}"
+#    oauth_scopes = [
+#      "https://www.googleapis.com/auth/compute",
+#      "https://www.googleapis.com/auth/devstorage.read_only",
+#      "https://www.googleapis.com/auth/logging.write",
+#      "https://www.googleapis.com/auth/monitoring"
+#    ]
+#  }
+#}
 
